0000513efcbf9c1b1f59a20b338c96182424826e
zchen9 <zxcvbnm.pop@qq.com>
2015-05-11 17:07:21 +0800

    fix bug & change highlight

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,34 @@ window.onload = function(){
 
 	var infoBtn = $("#info_buttom");
 	var pageWarp = $("#info-warp");
+	var homeBtn = $("#home_btn");
 
 	var listInfo = $("#list_info");
 	var listCon = $("#list_contact");
 	var listAbout = $("#list_about");
 
-	var scrollWidth = document.body.scrollWidth;
-	var scrollHeight = document.body.scrollHeight;
 
-	infoBtn.onclick = function(){
+	addEvent(infoBtn,"click",scrollDown);
+	addEvent(homeBtn,"click",scrollUp);
+
+	function scrollDown(){
+		var pageWarp = $("#info-warp");
 
 		var clientWidth = document.documentElement.clientWidth || document.body.offsetWidth;
 		var clientHeight = document.documentElement.clientHeight || document.body.offsetHeight;
 
 		if(pageWarp){
 			pageWarp.style.top = 0 +"px";
-			var pageWidth = Math.min(scrollWidth,clientWidth);
-			var pageHeight = Math.min(scrollHeight,clientHeight);
-			pageWarp.style.width = pageWidth + "px";
-			pageWarp.style.height = pageHeight + "px";
+			pageWarp.style.width = clientWidth + "px";
+			pageWarp.style.height = clientHeight + "px";
+		}
+
+	}
+
+	function scrollUp(){
+		var pageWarp = $("#info-warp");
+		if(pageWarp){
+			pageWarp.style.top = -200 +"%" ;
 		}
 	}
 };
