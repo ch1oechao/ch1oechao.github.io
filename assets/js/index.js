29f2b4470c182ce3870e4880f5a2fc38aee4356c
zchen9 <zxcvbnm.pop@qq.com>
2015-05-28 14:18:14 +0800

    fix bug~

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,76 @@
 $(document).ready(function(){
 
-	$("#list_info").bind("click",function(){
-		$(".info_me").addClass("info_show");
+    $("#list_info").bind("click",function(){
+        $("#list_info").addClass("active");
+        $("#list_contact,#list_about").removeClass("active");
+
+        $(".info_me").addClass("info_show");
+        $(".contact_me").removeClass("contact_show");
+        $(".about_here").removeClass("about_show");
     });
 
-	$("#list_contact").bind("click",function(){
-		$(".contact_me").addClass("contact_show");
-	});
+    $("#list_contact").bind("click",function(){
+        $("#list_contact").addClass("active");
+        $("#list_info,#list_about").removeClass("active");
 
-	$("#info_buttom").bind("click",scrollDown);
-	$("#home_btn").bind("click",scrollUp);
+        $(".contact_me").addClass("contact_show");
+        $(".info_me").removeClass("info_show");
+        $(".about_here").removeClass("about_show");
+    });
+
+    $("#list_about").bind("click",function(){
+        $("#list_about").addClass("active");
+        $("#list_info,#list_contact").removeClass("active");
+
+        $(".about_here").addClass("about_show");
+        $(".contact_me").removeClass("contact_show");
+        $(".info_me").removeClass("info_show");
+    });
+
+    $("#info_buttom").bind("click",scrollDown);
+    $("#home_btn").bind("click",scrollUp);
 
 
-	// 回到顶部
-	$("article").scroll(function(){		
-		$("article").scrollTop()>$("header").height()? $("#returnTop").css("bottom","80px"):$("#returnTop").css("bottom","-200px");
-	});
-	$(window).scroll(function(){
-		$(window).scrollTop()>$("article").height()? $("#returnTop").css("bottom","80px"):$("#returnTop").css("bottom","-200px");
-	});
-	$("#returnTop").bind("click",function(){
-		$("article").animate({scrollTop: 0},500),
-		$("html,body").animate({scrollTop: 0},500)
-	});
+    // 回到顶部
+    $("article").scroll(function(){     
+        $("article").scrollTop()>$("header").height()? $("#returnTop").css("bottom","80px"):$("#returnTop").css("bottom","-200px");
+    });
+    $(window).scroll(function(){
+        $(window).scrollTop()>$("article").height()? $("#returnTop").css("bottom","80px"):$("#returnTop").css("bottom","-200px");
+    });
+    $("#returnTop").bind("click",function(){
+        $("article").animate({scrollTop: 0},500),
+        $("html,body").animate({scrollTop: 0},500)
+    });
 
 });
 
 function scrollDown(){
-	var clientWidth = document.documentElement.clientWidth;
-	var scrollHeight = document.body.scrollHeight;
+    if($("#info-warp")){
+        $("#info-warp").css({
+            top: 0 + "px",
+            width: 100 + "%",
+            height: 100 + "%",
+            opacity: 1,
+            zIndex: 9999
+        });
 
-	if($("#info-warp")){
-		if(clientWidth < 980){
-			$("#info-warp").css("height",scrollHeight + "px");
-		}else{
-			$("#info-warp").css("height",100 + "%");
-		}
-		$("#info-warp").css({
-			top: 0 + "px",
-			width: 100+"%",
-			opacity: 1,
-			zIndex: 9999
-		});
-		$(".info_me").addClass("info_show");
-	}
+        $("#list_info").addClass("active");
+        $(".info_me").addClass("info_show");
+    }
 }
 
 function scrollUp(){
-	var scrollHeight = document.body.scrollHeight;
-	if($("#info-warp")){
-		$("#info-warp").css({
-			top: -scrollHeight +"px",
-			opacity: 0,
-			zIndex: -1
-		});
-	}
+    if($("#info-warp")){
+        $("#info-warp").css({
+            top: -$("html,body").height() +"px",
+            opacity: 0,
+            zIndex: -1
+        });
+    }
+
+    $("#list_info,#list_contact,#list_about").removeClass("active");
+    $(".info_me").removeClass("info_show");
+    $(".contact_me").removeClass("contact_show");
+    $(".about_here").removeClass("about_show");
 }
