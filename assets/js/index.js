aedd7a0dc13c4ab6637c438d4b5b264232e9af0a
zchen9 <zhao.zchen9@gmail.com>
2016-06-02 13:23:28 +0800

    fix mobile device display & filter info

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $('.logo-svg').toggleClass('switch');
     $('.logo-container').toggleClass('switch');
   }
-
+  
   $avatar.on('click', switchScreen);
 
   console.log('© zchen9 🙋 2015-' + (new Date()).getFullYear());
